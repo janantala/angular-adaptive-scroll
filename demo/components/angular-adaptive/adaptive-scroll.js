d13636707bc684b62360bc10508b46ab970ea0e5
Jan Antala <jan@antala.sk>
2013-06-22 13:33:02 +0200

    rotate right axes, requestAnimationFrame start after start clicked, cancel event

--- demo/components/angular-adaptive/adaptive-scroll.js
+++ demo/components/angular-adaptive/adaptive-scroll.js
@@ -9,50 +9,29 @@ adaptive.factory('$gyroscope', ['$rootScope', function ($rootScope) {
   var trashold = 20;
   var active;
 
-  window.ondevicemotion = function(event) {
-var ax = event.accelerationIncludingGravity.x;
-var ay = event.accelerationIncludingGravity.y;
-var az = event.accelerationIncludingGravity.z;
-var rotation = event.rotationRate;
-console.log(ax);
-console.log(ay);
-console.log(az);
-console.log(rotation);
-if (rotation != null) {
-  var arAlpha = Math.round(rotation.alpha);
-  var arBeta = Math.round(rotation.beta);
-  var arGamma = Math.round(rotation.gamma);
-  console.log(arAlpha);
-  console.log(arBeta);
-  console.log(arGamma);
-
-}
-  };
-
-
   window.ondeviceorientation = function(event) {
-    alert('ondeviceorientation');
+    console.log(event);
+    if (!active) {
+      return false;
+    }
+
     alpha = Math.round(event.alpha);
     beta = Math.round(event.beta);
     gamma = Math.round(event.gamma);
 
-    if (active && !alphaStart && !betaStart && !gammaStart) {
+    if (!alphaStart && !betaStart && !gammaStart) {
       alphaStart = alpha;
       betaStart = beta;
       gammaStart = gamma;
     }
 
-    console.log(alpha, beta, gamma);
-    console.log(Math.abs(alphaStart - alpha), Math.abs(betaStart - beta), Math.abs(gammaStart - gamma));
-    console.log(Math.abs(alphaStart - alpha) > trashold, Math.abs(betaStart - beta) > trashold, Math.abs(gammaStart - gamma) > trashold);
+    console.log('starting', alphaStart, betaStart, gammaStart);
+    console.log('current', alpha, beta, gamma);
+    console.log('diff', Math.abs(alphaStart - alpha), Math.abs(betaStart - beta), Math.abs(gammaStart - gamma));
+    console.log('passed trashold: ' + trashold, Math.abs(alphaStart - alpha) > trashold, Math.abs(betaStart - beta) > trashold, Math.abs(gammaStart - gamma) > trashold);
 
-    if (active) {
-      if ((Math.abs(alphaStart - alpha) > trashold) || (Math.abs(betaStart - beta) > trashold) || (Math.abs(gammaStart - gamma) > trashold)) {
-        sendEvent();
-      }
-      else {
-        sendEvent(0,0,0);
-      }
+    if ((Math.abs(alphaStart - alpha) > trashold) || (Math.abs(betaStart - beta) > trashold) || (Math.abs(gammaStart - gamma) > trashold)) {
+      sendEvent();
     }
   };
 
@@ -60,23 +39,30 @@ if (rotation != null) {
     var alphaDiff = a || (alphaStart - alpha);
     var betaDiff = b || (betaStart - beta);
     var gammaDiff = c || (gammaStart - gamma);
-    $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'alphaDiff': alphaDiff, 'betaDiff': betaDiff, 'gammaDiff': gammaDiff});
+    $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'event': 'ondeviceorientation', 'alphaDiff': alphaDiff, 'betaDiff': betaDiff, 'gammaDiff': gammaDiff});
+  };
+
+  var startEvent = function() {
+        $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'event': 'ondeviceorientationstart'});
+  };
+
+  var stopEvent = function() {
+        $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'event': 'ondeviceorientationstop'});
   };
 
   var watchPosition = function(degrees) {
-    console.log('w', degrees);
+    console.log('trashold', degrees);
     trashold = degrees || trashold;
     active = true;
   };
 
   var ignorePosition = function() {
     active = false;
-    sendEvent(0,0,0);
+    stopEvent();
   };
 
   return {
     watchPosition: function(degrees) {
-      console.log('rw', degrees);
       watchPosition(degrees);
     },
     ignorePosition: function() {
@@ -96,36 +82,63 @@ adaptive.directive('adaptivescroll', ['$rootScope', function ($rootScope) {
       var opts = getOptions();
       console.log(opts);
 
-      window.requestAnimationFrame = (function(){
-        return  window.requestAnimationFrame       ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame    ||
-                function( callback ){
-                  window.setTimeout(callback, 1000 / 60);
-                };
-      })();
+
+      (function() {
+        var lastTime = 0;
+        var vendors = ['webkit', 'moz'];
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+            window.cancelAnimationFrame =
+              window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                  timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+      }());
 
       var alphaDiff = 0;
       var betaDiff = 0;
       var gammaDiff = 0;
+      var requestId;
 
       var scroll = function() {
         if (alphaDiff || betaDiff) {
-          element[0].scrollTop = element[0].scrollTop + alphaDiff; 
-          element[0].scrollLeft = element[0].scrollLeft + betaDiff;
+          element[0].scrollTop = element[0].scrollTop + betaDiff;
+          element[0].scrollLeft = element[0].scrollLeft + gammaDiff;
         }
-        window.requestAnimationFrame(scroll);
+        requestId = window.requestAnimationFrame(scroll);
       };
-
-      window.requestAnimationFrame(scroll);
 
       $rootScope.$on('adaptive.scroll:deviceorientation', function(e, data){
 
-        alphaDiff = data.alphaDiff;
-        betaDiff = data.betaDiff;
-        gammaDiff = data.gammaDiff;
+        console.log('received', data);
 
-        console.log('received', {'alphaDiff': alphaDiff, 'betaDiff': betaDiff, 'gammaDiff': gammaDiff});
+        if (data.event == 'ondeviceorientation') {
+          alphaDiff = data.alphaDiff;
+          betaDiff = data.betaDiff;
+          gammaDiff = data.gammaDiff;
+        }
+
+        if (data.event == 'ondeviceorientation' && !requestId) {
+          requestId = window.requestAnimationFrame(scroll);
+        }
+
+        if (data.event == 'ondeviceorientationstop' && requestId) {
+          window.cancelAnimationFrame(requestId);
+          requestId = null;
+        }
 
       });
 
