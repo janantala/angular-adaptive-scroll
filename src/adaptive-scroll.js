73533336668b74d07abd16ece1c5fdb5df0eb25d
Jan Antala <jan@antala.sk>
2013-06-22 13:34:10 +0200

    master file

--- src/adaptive-scroll.js
+++ src/adaptive-scroll.js
@@ -10,21 +10,28 @@ adaptive.factory('$gyroscope', ['$rootScope', function ($rootScope) {
   var active;
 
   window.ondeviceorientation = function(event) {
+    console.log(event);
+    if (!active) {
+      return false;
+    }
+
     alpha = Math.round(event.alpha);
     beta = Math.round(event.beta);
     gamma = Math.round(event.gamma);
 
-    console.log(alpha, beta, gamma);
-    console.log(Math.abs(alphaStart - alpha), Math.abs(betaStart - beta), Math.abs(gammaStart - gamma));
-    console.log(Math.abs(alphaStart - alpha) > trashold, Math.abs(betaStart - beta) > trashold, Math.abs(gammaStart - gamma) > trashold);
+    if (!alphaStart && !betaStart && !gammaStart) {
+      alphaStart = alpha;
+      betaStart = beta;
+      gammaStart = gamma;
+    }
 
-    if (active) {
-      if ((Math.abs(alphaStart - alpha) > trashold) || (Math.abs(betaStart - beta) > trashold) || (Math.abs(gammaStart - gamma) > trashold)) {
-        sendEvent();
-      }
-      else {
-        sendEvent(0,0,0);
-      }
+    console.log('starting', alphaStart, betaStart, gammaStart);
+    console.log('current', alpha, beta, gamma);
+    console.log('diff', Math.abs(alphaStart - alpha), Math.abs(betaStart - beta), Math.abs(gammaStart - gamma));
+    console.log('passed trashold: ' + trashold, Math.abs(alphaStart - alpha) > trashold, Math.abs(betaStart - beta) > trashold, Math.abs(gammaStart - gamma) > trashold);
+
+    if ((Math.abs(alphaStart - alpha) > trashold) || (Math.abs(betaStart - beta) > trashold) || (Math.abs(gammaStart - gamma) > trashold)) {
+      sendEvent();
     }
   };
 
@@ -32,27 +39,34 @@ adaptive.factory('$gyroscope', ['$rootScope', function ($rootScope) {
     var alphaDiff = a || (alphaStart - alpha);
     var betaDiff = b || (betaStart - beta);
     var gammaDiff = c || (gammaStart - gamma);
-    $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'alphaDiff': alphaDiff, 'betaDiff': betaDiff, 'gammaDiff': gammaDiff});
+    $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'event': 'ondeviceorientation', 'alphaDiff': alphaDiff, 'betaDiff': betaDiff, 'gammaDiff': gammaDiff});
   };
 
-  var start = function(degrees) {
-    alphaStart = alpha;
-    betaStart = beta;
-    gammaStart = gamma;
+  var startEvent = function() {
+        $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'event': 'ondeviceorientationstart'});
+  };
+
+  var stopEvent = function() {
+        $rootScope.$broadcast('adaptive.scroll:deviceorientation', {'event': 'ondeviceorientationstop'});
+  };
+
+  var watchPosition = function(degrees) {
+    console.log('trashold', degrees);
     trashold = degrees || trashold;
     active = true;
   };
 
-  var stop = function() {
+  var ignorePosition = function() {
     active = false;
+    stopEvent();
   };
 
   return {
-    start: function() {
-      start();
+    watchPosition: function(degrees) {
+      watchPosition(degrees);
     },
-    stop: function() {
-      stop();
+    ignorePosition: function() {
+      ignorePosition();
     }
   };
 
@@ -68,32 +82,64 @@ adaptive.directive('adaptivescroll', ['$rootScope', function ($rootScope) {
       var opts = getOptions();
       console.log(opts);
 
-      window.requestAnimationFrame = (function(){
-        return  window.requestAnimationFrame       ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame    ||
-                function( callback ){
-                  window.setTimeout(callback, 1000 / 60);
-                };
-      })();
+
+      (function() {
+        var lastTime = 0;
+        var vendors = ['webkit', 'moz'];
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+            window.cancelAnimationFrame =
+              window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                  timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+      }());
 
       var alphaDiff = 0;
       var betaDiff = 0;
       var gammaDiff = 0;
+      var requestId;
 
       var scroll = function() {
         if (alphaDiff || betaDiff) {
-          element.scrollTop = element.scrollTop + alphaDiff; 
-          element.scrollLeft = element.scrollLeft + betaDiff;
+          element[0].scrollTop = element[0].scrollTop + betaDiff;
+          element[0].scrollLeft = element[0].scrollLeft + gammaDiff;
         }
+        requestId = window.requestAnimationFrame(scroll);
       };
 
-      window.requestAnimationFrame(scroll);
-
       $rootScope.$on('adaptive.scroll:deviceorientation', function(e, data){
-        alphaDiff = data.alphaDiff;
-        betaDiff = data.betaDiff;
-        gammaDiff = data.gammaDiff;
+
+        console.log('received', data);
+
+        if (data.event == 'ondeviceorientation') {
+          alphaDiff = data.alphaDiff;
+          betaDiff = data.betaDiff;
+          gammaDiff = data.gammaDiff;
+        }
+
+        if (data.event == 'ondeviceorientation' && !requestId) {
+          requestId = window.requestAnimationFrame(scroll);
+        }
+
+        if (data.event == 'ondeviceorientationstop' && requestId) {
+          window.cancelAnimationFrame(requestId);
+          requestId = null;
+        }
+
       });
 
     }
