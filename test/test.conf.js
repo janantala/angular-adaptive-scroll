6fdc96b51613bd6b831d0738a5789452e6d413d1
Jan Antala <jan@antala.sk>
2013-08-10 23:06:38 +0200

    frameworks jasmine

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,11 +1,10 @@
 basePath = '..';
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
   'src/adaptive-scroll.js',
   'test/*.spec.js'
 ];
+frameworks = ["jasmine"];
 singleRun = true;
 browsers = [ 'Chrome' ];
