58f1baf3e15765f29354e5d3cc21127ce92d14c7
Jan Antala <jan@antala.sk>
2013-08-10 23:16:28 +0200

    change karma config as module.exports

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,10 +1,14 @@
-basePath = '..';
-files = [
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'src/adaptive-scroll.js',
-  'test/*.spec.js'
-];
-frameworks = ["jasmine"];
-singleRun = true;
-browsers = [ 'Chrome' ];
+module.exports = function(config) {
+  config.set({
+    basePath: '..',
+    files: [
+      'components/angular/angular.js',
+      'components/angular-mocks/angular-mocks.js',
+      'src/adaptive-scroll.js',
+      'test/*.spec.js'
+    ],
+    frameworks: ['jasmine'],
+    singleRun: true,
+    browsers: [ 'Chrome' ]
+  });
+};
